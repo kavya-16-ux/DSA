1463d31853415a1cbb2d99996cf92268b414a7eb
Kavya <kavyagp32@gmail.com>
2026-09-20 23:37:53 +0530

    PatternsProblems

---
+++ Patterns1/P3.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class P3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n value:");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(i);
+            }
+            System.out.println(); // moves to next line
+        }
+        sc.close();
+    }
+}
---
+++ Patterns1/P5.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class P5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n value:");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // moves to next line
+        }
+        sc.close();
+    }
+}
