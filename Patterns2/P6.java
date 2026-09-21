28314a3e5813b38645478940398043bb96a42103
Kavya <kavyagp32@gmail.com>
2026-09-21 22:58:53 +0530

    PatternsProblems

---
+++ Patterns2/P6.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class P6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        int m = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i + (i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (m - i) + (m - i + 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
